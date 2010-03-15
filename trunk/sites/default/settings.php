08dcdfde0b4e1a19e22c7f854a22d50c0168e670
mradomar@gmail.com <mradomar@gmail.com@e1dac8ea-2327-11df-99a1-e5276f3be39f>
2010-03-15 20:51:08 +0000

    Importante: APLICO EL CAMBIO EN LA BASE DE DATOS... AHORA APUNTA AFUERA... NO USA MAS EL LOCALHOST!!!!

--- trunk/sites/default/settings.php
+++ trunk/sites/default/settings.php
@@ -90,7 +90,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://root@localhost/empleos';
+$db_url = 'mysql://empleos:3mpl30s@64.191.99.172/empleos';
 $db_prefix = '';
 
 /**
