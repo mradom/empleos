02ca0ee7e8c83f5219154d5e87cc68e0041c8ca5
mradomar@gmail.com <mradomar@gmail.com@e1dac8ea-2327-11df-99a1-e5276f3be39f>
2010-04-19 22:02:10 +0000

    head con modificaciones para que no se pise el jquery y prototype

--- trunk/sites/all/themes/empleos/include/head.php
+++ trunk/sites/all/themes/empleos/include/head.php
@@ -19,9 +19,12 @@
 <script type="text/javascript" src="/sites/all/modules/jquery_ui/jquery.ui/ui/ui.datepicker.js"></script>
 <script type="text/javascript" src="/sites/all/modules/jquery_update/compat.js"></script>
 <script type="text/javascript" src="/sites/all/themes/empleos/js/jquery.innerfade.js"></script>
+<script type="text/javascript" src="/sites/all/themes/empleos/js/jquery.innerfade.page.js"></script>
+
+<?php if (arg(0) == "principal") {?>
 <script type="text/javascript" src="/sites/all/themes/empleos/js/prototype.js" ></script>
 <script type="text/javascript" src="/sites/all/themes/empleos/js/effects.js" ></script>
 <script type="text/javascript" src="/sites/all/themes/empleos/js/glider.js" ></script>
 <script type="text/javascript" src="/sites/all/themes/empleos/js/scriptPag.js" ></script>
-<script type="text/javascript" src="/sites/all/themes/empleos/js/jquery.innerfade.page.js"></script>
+<?php }?>
 </head>
